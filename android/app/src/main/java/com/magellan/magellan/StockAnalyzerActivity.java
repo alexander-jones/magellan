a05b79a1cb586104c5944a279aee0cab1301708c
Alexander Jones <jonesalexandereng@gmail.com>
2017-08-14 20:55:49 -0700

    add some asset files and modify theming further

--- android/app/src/main/java/com/magellan/magellan/StockAnalyzerActivity.java
+++ android/app/src/main/java/com/magellan/magellan/StockAnalyzerActivity.java
@@ -379,17 +379,15 @@ public class StockAnalyzerActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.quotes) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.earnings) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.notifications) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.contribute) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.donate) {
 
         }
 
