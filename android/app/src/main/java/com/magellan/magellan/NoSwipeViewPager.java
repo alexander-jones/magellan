1edf58ac4a52a4653237b6e0726edf1622f32f14
Alexander Jones <jonesalexandereng@gmail.com>
2017-09-24 22:38:24 -0700

    Disable swiping on view pager and make portfolio tab smaller.

---
+++ android/app/src/main/java/com/magellan/magellan/NoSwipeViewPager.java
@@ -0,0 +1,29 @@
+package com.magellan.magellan;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NoSwipeViewPager extends ViewPager {
+    private Boolean mScrollingEnabled = false;
+    public NoSwipeViewPager(Context context) {
+        super(context);
+    }
+    public NoSwipeViewPager(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return mScrollingEnabled ? super.onInterceptTouchEvent(event) : false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mScrollingEnabled ?  super.onTouchEvent(event) : false;
+    }
+
+    public void setScrollEnabled(Boolean enable){
+        mScrollingEnabled = enable;
+    }
+}
